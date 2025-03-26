397981a0756e36848a0fc8a08e519e38e5eeb6a5
lenonmerlo <lenontm@gmail.com>
2025-03-26 07:37:57 -0300

    Product

---
+++ src/main/java/com/devlenonmerlo/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.devlenonmerlo.course.services;
+
+import com.devlenonmerlo.course.entities.Product;
+import com.devlenonmerlo.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
+
+
