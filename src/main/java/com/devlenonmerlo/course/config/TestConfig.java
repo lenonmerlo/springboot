eb9d3b8f6bec22af2ea4adfa25ed7ebe7513ca5f
lenonmerlo <lenontm@gmail.com>
2025-03-26 07:19:19 -0300

    Category

--- src/main/java/com/devlenonmerlo/course/config/TestConfig.java
+++ src/main/java/com/devlenonmerlo/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.devlenonmerlo.course.config;
 
+import com.devlenonmerlo.course.entities.Category;
 import com.devlenonmerlo.course.entities.Order;
 import com.devlenonmerlo.course.entities.User;
 import com.devlenonmerlo.course.entities.enums.OrderStatus;
+import com.devlenonmerlo.course.repositories.CategoryRepository;
 import com.devlenonmerlo.course.repositories.OrderRepository;
 import com.devlenonmerlo.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
