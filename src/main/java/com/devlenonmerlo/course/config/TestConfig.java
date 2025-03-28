1a2ac51943c5def4fb2e2fc65dd357ca7766e300
lenonmerlo <lenontm@gmail.com>
2025-03-28 08:45:58 -0300

    OrderItem, many-to-many association with extra attributes - Part 2

--- src/main/java/com/devlenonmerlo/course/config/TestConfig.java
+++ src/main/java/com/devlenonmerlo/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.devlenonmerlo.course.config;
 
-import com.devlenonmerlo.course.entities.Category;
-import com.devlenonmerlo.course.entities.Order;
-import com.devlenonmerlo.course.entities.Product;
-import com.devlenonmerlo.course.entities.User;
+import com.devlenonmerlo.course.entities.*;
 import com.devlenonmerlo.course.entities.enums.OrderStatus;
-import com.devlenonmerlo.course.repositories.CategoryRepository;
-import com.devlenonmerlo.course.repositories.OrderRepository;
-import com.devlenonmerlo.course.repositories.ProductRepository;
-import com.devlenonmerlo.course.repositories.UserRepository;
+import com.devlenonmerlo.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -68,5 +65,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
---
+++ src/main/java/com/devlenonmerlo/course/repositories/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.devlenonmerlo.course.repositories;
+
+import com.devlenonmerlo.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
