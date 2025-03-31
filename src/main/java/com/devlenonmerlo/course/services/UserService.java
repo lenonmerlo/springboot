5908107247a3762a237697bcf958e525a6649859
lenonmerlo <lenontm@gmail.com>
2025-03-31 07:35:07 -0300

    Exception handling - delete

--- src/main/java/com/devlenonmerlo/course/services/UserService.java
+++ src/main/java/com/devlenonmerlo/course/services/UserService.java
@@ -2,8 +2,10 @@ package com.devlenonmerlo.course.services;
 
 import com.devlenonmerlo.course.entities.User;
 import com.devlenonmerlo.course.repositories.UserRepository;
+import com.devlenonmerlo.course.services.exceptions.DatabaseException;
 import com.devlenonmerlo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,8 +31,17 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            if (!repository.existsById(id)) {
+                throw new ResourceNotFoundException(id);
+            }
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
+
+
 
     public User update(Long id, User obj) {
         User entity = repository.getReferenceById(id);
--- src/main/java/com/devlenonmerlo/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/devlenonmerlo/course/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,11 @@
 package com.devlenonmerlo.course.services.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.io.Serial;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     @Serial
